1544610cc430f75a96f67e17af0a6da56885a6d7
Kaic Diniz Teodoro <kaic.teodoro@etec.sp.gov.br>
2020-10-27 22:25:23 -0300

    Concluso o ex passado dia 27/10/2020

--- Dani/classe.js
+++ Dani/classe.js
@@ -39,3 +39,36 @@ let obj2 = new Veiculo("Fiat", "Palio", 2020, false,70)
 obj2.veloDim(30)
 obj2.mostra()
 obj2.on()
+
+// 27-10-2020 
+
+class Aluno {
+constructor(num, nome, idade, p1, p2, pas){
+    this.num = num
+    this.nome = nome
+    this.idade = idade
+    this.p1 = p1
+    this.p2 = p2
+    this.pas = pas
+}
+
+notaFinal = () => {
+return (this.p1 + this.p2)/2
+//console.log(`Nota final do aluno ${this.nome}: ${this.med};`)
+}
+passou = (not) => {
+this.pas = (not>5)? "Aprovado" : "Reprovado"
+}
+
+mostra = () => {
+    console.log(`Número do Aluno: ${this.num};`)
+    console.log(`Nome do Aluno: ${this.nome};`)
+    console.log(`Idade do Aluno: ${this.idade};`)
+    let media = this.notaFinal()
+    this.passou(media)
+    console.log(`Média do Aluno: ${media};`)
+    console.log(`Situação do Aluno: ${this.pas};`)
+}
+}
+let obj1 = new Aluno(20, "kaic", 18, 8, 10, "Não avaliado")
+obj1.mostra()
